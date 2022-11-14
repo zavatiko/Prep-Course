ef6cc40113e108c066b23ccc41b7fccfc205ab79
zavatiko <jeremias.zavala96@gmail.com>
2022-11-14 16:03:05 -0300

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,13 +175,20 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + ! 
+
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var nombre = henry ; {
+    var apellido = ortega ;
+    return nombre + apellido ;
+    
+  }
+
 }
 
 function obtenerSaludo(nombre) {
